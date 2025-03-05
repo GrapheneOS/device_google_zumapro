c6a9d4f6bc8fdf653ed4b28493199441b4136bb9
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-03-05 18:59:01 +0200

    fixup! add missing FCM definitions for some proprietary HALs

--- device.mk
+++ device.mk
@@ -1292,7 +1292,6 @@ PRODUCT_PACKAGES_DEBUG += BatteryStatsViewer
 
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/gs-common/proprietary/android.hardware.gnss.xml
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/gs-common/proprietary/vendor.google.aam.xml
-DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/gs-common/proprietary/vendor.google.whitechapel.audio.extension.xml
 
 # Keymint configuration
 PRODUCT_COPY_FILES += \
