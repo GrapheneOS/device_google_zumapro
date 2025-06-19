487dcb59dc3cb9467e3f921e005cb7b4fbe347a5
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-19 15:50:10 +0000

    strip hwc as //hardware/google/graphics is dropped upstream

--- device.mk
+++ device.mk
@@ -856,7 +856,6 @@ PRODUCT_PACKAGES += \
 
 $(call soong_config_set,google_displaycolor,displaycolor_platform,zuma)
 PRODUCT_PACKAGES += \
-	android.hardware.composer.hwc3-service.pixel \
 	libdisplaycolor \
 	libdisplaypanel
 
