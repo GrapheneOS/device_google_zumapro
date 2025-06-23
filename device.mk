e1a2c093741cb0335cc3db00d87430b6afb0dcb1
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-23 09:51:32 +0000

    strip FUSE passthrough props

--- device.mk
+++ device.mk
@@ -614,10 +614,6 @@ PRODUCT_PACKAGES += \
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Use FUSE passthrough
-PRODUCT_PRODUCT_PROPERTIES += \
-	persist.sys.fuse.passthrough.enable=true
-
 # Use /product/etc/fstab.postinstall to mount system_other
 PRODUCT_PRODUCT_PROPERTIES += \
 	ro.postinstall.fstab.prefix=/product
